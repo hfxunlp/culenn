ec7edc9f520185fb6dd9d33d890dd48c0407f33d
ano <hfxunlp@foxmail.com>
2017-11-22 09:35:47 +0800

    further test

--- test/lensoftmax.lua
+++ test/lensoftmax.lua
@@ -2,6 +2,7 @@ require "cunn"
 require "culenn"
 tmodstd=nn.SoftMax():cuda()
 tmod=lenn.LenSoftMax():cuda()
+tmodstd1=nn.SoftMax():cuda()
 minbsize=8
 maxbsize=64
 minlen=16
@@ -15,22 +16,32 @@ for t=1, firstcycle do
 		bsize=math.random(minbsize, maxbsize)
 		lens=math.random(minlen, maxlen)
 		plens=math.random(minpadlen, maxpadlen)
+		eplens=math.random(lens+1,plens+lens-1)
 		lvec=torch.LongTensor(bsize):fill(lens):cudaLong()
+		lvec[bsize]=eplens
 		stdi=torch.randn(bsize, lens):cuda()
 		i=torch.cat(stdi, torch.randn(bsize, plens):cuda())
 		stdgo=torch.randn(bsize, lens):cuda()
 		go=torch.cat(stdgo, torch.randn(bsize, plens):cuda())
-		stdo=tmodstd:forward(stdi)
+		stdo=tmodstd:forward(stdi:narrow(1,1,bsize-1))
+		stdo1=tmodstd1:forward(i[-1]:narrow(1, 1, eplens))
 		o=tmod:forward({i, lvec})
-		if not (o:narrow(2, 1, lens):equal(stdo) and o:narrow(2, lens+1, plens):equal(torch.zeros(bsize, plens):cuda()) ) then
+		if not (o[-1]:narrow(1, 1, eplens):equal(stdo1) and o[-1]:narrow(1, eplens+1, plens+lens-eplens):equal(torch.zeros(plens+lens-eplens):cuda()) and o:narrow(1,1,bsize-1):narrow(2, 1, lens):equal(stdo) and o:narrow(1,1,bsize-1):narrow(2, lens+1, plens):equal(torch.zeros(bsize-1, plens):cuda())) then
 			psg=false
 			print("forward error")
+			print(stdo)
+			print(stdo1)
+			print(o)
 		end
-		stdgi=tmodstd:backward(stdi, stdgo)
+		stdgi=tmodstd:backward(stdi:narrow(1,1,bsize-1), stdgo:narrow(1,1,bsize-1))
+		stdgi1=tmodstd1:backward(i[-1]:narrow(1, 1, eplens), go[-1]:narrow(1, 1, eplens))
 		gi=tmod:backward({i, lvec}, go)[1]
-		if not (gi:narrow(2, 1, lens):equal(stdgi) and gi:narrow(2, lens+1, plens):equal(torch.zeros(bsize, plens):cuda()) ) then
+		if not (gi[-1]:narrow(1, 1, eplens):equal(stdgi1) and gi[-1]:narrow(1, eplens+1, plens+lens-eplens):equal(torch.zeros(plens+lens-eplens):cuda()) and gi:narrow(1,1,bsize-1):narrow(2, 1, lens):equal(stdgi) and gi:narrow(1,1,bsize-1):narrow(2, lens+1, plens):equal(torch.zeros(bsize-1, plens):cuda())) then
 			psg=false
 			print("backward error")
+			print(stdgi)
+			print(stdgi1)
+			print(gi)
 		end
 		xlua.progress(t, firstcycle)
 	end
